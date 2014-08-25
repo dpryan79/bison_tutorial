4f033d5278bc1c194954feb22142b0c7def6a12e
dpryan79 <dpryan@dpryan.com>
2014-08-25 14:54:55 +0200

    Add a bit of content to README.md

--- generate.sh
+++ generate.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-#for f in ../txt/*.txt
-#do
-#    prefix=${f%%.txt}
-#    of=`basename $prefix`
-#    asciidoc -n -a icons -a toc -o $of.html $f
-#done
-#cp -rf /usr/share/asciidoc/images ./
-#cat ../txt/*.txt > foo.txt
 a2x --format=chunked -a icons -a toc -d book -f chunked --icons -D ./ txt/combined.txt
 mv combined.chunked html
